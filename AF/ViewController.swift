c86a1d4631ea280416b5f2a3033d64b0f8069f8e
ali Fouad <alifouad@alis-macbook-pro.local>
2016-06-18 01:47:23 +0400

    Initial Commit

---
+++ AF/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  AF
+//
+//  Created by ali Fouad on 6/18/16.
+//  Copyright © 2016 AF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        example("hi!", p2: 0)
+        
+    }
+/**
+ This is ali
+ */
+    func example(p1:String, p2:Int){
+    
+    
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
